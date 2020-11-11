160339136d2843160a76e26246fc65a5ce7f9afe
monicaleep <monica.parrillo@gmail.com>
2020-11-10 20:47:36 -0600

    fix bug with zero score, add capability to delete profile.

--- controllers/profile.js
+++ controllers/profile.js
@@ -4,16 +4,33 @@ const db = require('../models');
 const isLoggedIn = require('../middleware/isLoggedIn');
 
 router.get('/',isLoggedIn,async (req,res)=>{
-//  const user = await db.user.findByPk(req.user.id)
   const usersEntries = await db.entry.findAll({
     where: {userId:req.user.id}
   })
   const totalScore = usersEntries.reduce((acc,cv)=>{
     return acc + cv.score
   }, 0);
-  const averageScore = totalScore/usersEntries.length;
-  //res.send("average score: "+totalScore/usersEntries.length)
+  let averageScore = totalScore !== 0 ? (totalScore/usersEntries.length).toFixed(2) : 0;
   res.render('profile',{happiness: averageScore, totalEntries: usersEntries.length})
 })
+
+
+router.delete('/:userid', isLoggedIn, async (req,res)=>{
+  try{
+    if (req.user.id == req.params.userid){
+      await db.user.destroy({
+        where: {id: req.user.id}
+      });
+      req.flash('success', 'account deleted');
+      res.redirect('/')
+    } else{
+      res.redirect('/')
+    }
+  } catch(err){
+    console.log(err);
+    res.redirect('/')
+  }
+})
+
 
 module.exports = router;

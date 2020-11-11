a96b6ed40edd9725f9676df28607933d9b02aab7
Monica <monica.parrillo@gmail.com>
2020-11-11 17:03:28 -0600

    refactor todo controller to use async

--- controllers/todo.js
+++ controllers/todo.js
@@ -4,61 +4,76 @@ const db = require('../models');
 const isLoggedIn = require('../middleware/isLoggedIn');
 
 // index route, show all tasks for the logged in user
-router.get('/',isLoggedIn,(req,res)=>{
-  db.user.findByPk(req.user.id)
-  .then(user=>{
-    user.getTodos({order: [['lastdone','DESC']]}).then(todos=>{
-      res.render('todo/index',{foundTodos:todos})
+router.get('/', isLoggedIn, async (req, res) => {
+  try {
+    const user = await db.user.findByPk(req.user.id)
+    const todos = await user.getTodos({
+      order: [
+        ['lastdone', 'DESC']
+      ]
     })
-  })
-  .catch(err=>{
+    res.render('todo/index', {
+      foundTodos: todos
+    })
+  } catch (err) {
     console.log(err)
-  })
+    res.redirect('/todo')
+  }
 })
 
 // new route - render new form
-router.get('/new',isLoggedIn,(req,res)=>{
+router.get('/new', isLoggedIn, (req, res) => {
   res.render('todo/new')
 })
 
 // update route - change date of last done task to today's date
-router.put('/:id',isLoggedIn,(req,res)=>{
-  const today = new Date(); 
-  db.todo.update({
-    lastdone: today,
-  },{
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(numRowsUpdated=>{
+router.put('/:id', isLoggedIn, async (req, res) => {
+  const today = new Date();
+  try {
+    await db.todo.update({
+      lastdone: today,
+    }, {
+      where: {
+        id: req.params.id
+      }
+    })
     res.redirect('/todo')
-  })
-
+  } catch (err) {
+    console.log(err);
+    res.redirect('/todo')
+  }
 })
 
 // create route - create a new task
-router.post('/',isLoggedIn,(req,res)=>{
-  db.todo.create({
-    task: req.body.task,
-    userId: req.user.id,
-    lastdone: null,
-  })
-  .then((createdTask)=>{
-    req.flash(`success`,`Added ${req.body.task} to your ideas list!`)
+router.post('/', isLoggedIn, async (req, res) => {
+  try {
+    const createdTask = await db.todo.create({
+      task: req.body.task,
+      userId: req.user.id,
+      lastdone: null,
+    })
+    req.flash(`success`, `Added "${req.body.task}" to your ideas list!`)
     res.redirect('/todo')
-  })
+  } catch (err) {
+    console.log(err)
+    res.redirect('/todo')
+  }
 })
 
 
 // delete route - delete a task from DB
-router.delete('/:id',isLoggedIn,(req,res)=>{
-  db.todo.destroy({
-    where: {id: req.params.id}
-  })
-  .then(numRowsDeleted=>{
+router.delete('/:id', isLoggedIn, async (req, res) => {
+  try{
+    await db.todo.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
     res.redirect('/todo')
-  })
+  } catch(err){
+    console.log(err)
+    res.redirect('/todo')
+  }
 })
 
 module.exports = router;
